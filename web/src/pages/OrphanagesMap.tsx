9d1d4e384db4a6d333e69bec9b77fe093a3de728
gustavo867 <tartatavo3@gmail.com>
2020-10-13 17:02:08 -0300

    :wrench: fixing dark mode

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -41,7 +41,7 @@ const OrphanagesMap: React.FC = () => {
       >
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/${
-            dark === true ? "dark" : "light"
+            dark ? "dark" : "light"
           }-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${
             process.env.REACT_APP_MAPBOX_TOKEN
           }`}
